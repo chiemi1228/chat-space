eb600d5cfef3cf7594826561e9bcbe05212dfe45
chiemi1228 <sucreunpeu@yahoo.co.jp>
2019-06-10 22:02:49 +0900

    Update message.js
    ・重複コードを変数に。
    ・if文を三項演算子に。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,65 +1,45 @@
 function buildHTML(message){
-    if ( message.image ) {
-      var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="message__info">
-            <div class="message__info__who">
-              ${message.user_name}
+    var message_first = ( message.image )? `<img src=${message.image} ></div></div>` : "";
+    var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="message__info">
+              <div class="message__info__who">
+                ${message.user_name}
+              </div>
+              <div class="message__info__when">
+                ${message.date}
+              </div>
             </div>
-            <div class="message__info__when">
-              ${message.date}
+            <div class="lower-message">
+              <p class="lower-message__content">
+              </p>
+              ${message_first}
             </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-            <img src=${message.image} >
-          </div>
-        </div>`
-      return html;
-    } else {
-      var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="message__info">
-            <div class="message__info__who">
-              ${message.user_name}
-            </div>
-            <div class="message__info__when">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
+          </div>`
+        return html;
   }
-  $(function(){
-    $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action')
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.messages').append(html);
-        $('html,body').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-        $('form')[0].reset();
-      })
-      .fail(function(){
-        alert('error');
+    $(function(){
+      $('#new_message').on('submit', function(e){
+          e.preventDefault();
+          var formData = new FormData(this);
+          var url = $(this).attr('action')
+          $.ajax({
+              url: url,
+              type: "POST",
+              data: formData,
+              dataType: 'json',
+              processData: false,
+              contentType: false
+            })
+              .done(function(data){
+                var html = buildHTML(data);
+                $('.messages').append(html);
+                $('html,body').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+                $('form')[0].reset();
+              })
+                .fail(function(){
+                  alert('error');
+                });
+                return false;
       });
-      return false;
-    });
-  })
+    }) 
