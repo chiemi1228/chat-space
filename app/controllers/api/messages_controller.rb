e2efa2b3370c0d5ab17e3ea489ed8fefb17977c0
chiemi1228 <sucreunpeu@yahoo.co.jp>
2019-06-19 19:26:19 +0900

    Update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,8 +2,8 @@ class Api::MessagesController < ApplicationController
     def index
         respond_to do |format|
             format.html
-            format.json { @messages = Message.where('id > ?', params[:id])}
-            
+            format.json { @messages = Message.where('id > ?', params[:id]).find(group_id: params[:group_id)}
+
                  # @messages = idが、params[:id](前回までで最新の.message　の　id)より大きいもの全て
                  # api/index.json.jbuilder に送る
     end
